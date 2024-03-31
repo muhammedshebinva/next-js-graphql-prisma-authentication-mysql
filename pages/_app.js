d217915546ee1b6652eead7b3fd5af5e9454750b
muhammedshebinva <muhammedshebinva@gmail.com>
2024-03-31 13:44:25 +0530

    added env this is for learning

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from '@apollo/client';
 
 //const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' }); // Replace with your backend GraphQL endpoint
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3001/api/graphql' });
+const httpLink = new HttpLink({ uri: 'http://localhost:3000/api/graphql' });
 
 const client = new ApolloClient({
   link: httpLink,
